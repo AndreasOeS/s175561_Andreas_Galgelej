166e1560d10ba0645594e2f768dde420f08ee460
Andreas <mail.Tailspot@gmail.com>
2019-11-11 11:57:28 +0100

    Lavet vundet spil skærm færdig

--- app/src/main/java/com/example/s175561andreasgalgelej/SpilVundet.java
+++ app/src/main/java/com/example/s175561andreasgalgelej/SpilVundet.java
@@ -3,6 +3,9 @@ package com.example.s175561andreasgalgelej;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SpilVundet extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SpilVundet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spil_vundet);
+
+        ArrayList <String> brugteBogstaver = Spil.gl.getBrugteBogstaver();
+
+        int forsøg = brugteBogstaver.size();
+
+        String forsøgString = String.valueOf(forsøg);
+
+        TextView antalForsøg = (TextView) findViewById(R.id.textView8);
+
+        antalForsøg.setText("Antal forsøg: " + forsøgString);
     }
 }
